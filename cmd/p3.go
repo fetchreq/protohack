a35a9bfc9b25ef71e02d2e5f399f9a30f02ac063
Ryan <rjprice04@gmail.com>
2023-12-23 14:08:02 -0600

    Problem 3 Done

--- cmd/p3.go
+++ cmd/p3.go
@@ -1,11 +1,17 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"strings"
+	"time"
+	"unicode"
 
 	"github.com/spf13/cobra"
 )
@@ -13,28 +19,196 @@ import (
 // p3Cmd represents the p3 command
 var p3Cmd = &cobra.Command{
 	Use:   "p3",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Budget Chat",
+	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("p3 called")
+
+		// Set up logger	
+		jsonHandler := slog.NewJSONHandler(os.Stderr, nil)
+		myslog := slog.New(jsonHandler)
+		myslog.Info("Starting Budget Chat")
+
+		listener, err := net.Listen("tcp", "0.0.0.0:10000")
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+		defer listener.Close()
+
+
+		channel := make(chan string)
+
+		go chatroom(myslog)
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error: ", err)
+				continue
+			}
+
+			go chatRoomHandler(conn, myslog, channel)
+
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(p3Cmd)
+}
 
-	// Here you will define your flags and configuration settings.
+const helloMessage = "Welcome to budgetchat! What shall I call you?"
+type ClientStatus  int
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// p3Cmd.PersistentFlags().String("foo", "", "A help for foo")
+const (
+	Joining ClientStatus = iota
+	Joined 
+)
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// p3Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+type EventType  int
+
+const (
+	NewMessage EventType = iota
+	NewUserBroadCast
+	UserList
+	UserLeave
+)
+
+var (
+	// Send a channel here to get room events back.  It will send the entire
+	// archive initially, and then new messages as they come in.
+	subscribe = make(chan Subscription)
+	// Send a channel here to unsubscribe.
+	unsubscribe = make(chan Subscription)
+	// Send events here to publish them.
+	publish = make(chan Event)
+)
+func chatroom (logger *slog.Logger) {
+	cliensts := make(map[Subscription]bool)
+	for {
+		select {
+		case sub := <- subscribe: 
+			logger.Info(fmt.Sprintf("[new user] %s", sub.name))	
+			var nameList strings.Builder
+			for user := range cliensts {
+				nameList.WriteString(fmt.Sprintf("%s, ", user.name))
+			}
+
+			names := nameList.String()
+			names = strings.TrimSuffix(names, ", ")
+			
+			sub.channel <- Event{eventType: UserList, message: names}
+			for user := range cliensts {
+
+				user.channel <- Event{eventType: NewUserBroadCast, message: sub.name}
+			}
+			cliensts[sub] = true
+
+		case event := <- publish:
+			for user := range cliensts {
+				if event.userId != user.id {
+					user.channel <- Event{eventType: NewMessage, message: event.message, name: event.name}
+				}
+			}
+		case unsub := <- unsubscribe: 
+			delete(cliensts, unsub)	
+			logger.Info(fmt.Sprintf("[user left] %s", unsub.name))	
+			for user := range cliensts {
+
+				user.channel <- Event{eventType: UserLeave, message: unsub.name}
+			}
+
+		}
+			
+		
+	}
+}
+
+type Subscription struct {
+	id string
+	name string
+	channel chan Event 
+}
+
+type Event struct {
+	userId string
+	name string
+	eventType EventType
+	message string
+}
+
+func chatRoomHandler(conn net.Conn, logger *slog.Logger, channel chan string) {
+	var user Subscription
+	defer conn.Close()
+	msg := []byte(helloMessage)
+	msg = append(msg, '\n')
+	conn.Write(msg)
+	
+	c := make(chan Event)
+	scanner := bufio.NewScanner(conn)
+	status := Joining
+	for scanner.Scan() {
+		buffer := scanner.Bytes()
+		
+		msg := string(buffer)
+		if status == Joining {
+			allowed := true
+			for _, letter := range msg {
+				if !unicode.IsLetter(letter) && !unicode.IsDigit(letter) && letter != '\n' {
+					allowed = false	
+				}
+			}
+			if len(strings.TrimSpace(msg)) == 0 || len(msg) < 1 || !allowed {
+				break
+			}
+
+			user = Subscription{id: fmt.Sprintf("%s_%s_%s", time.Nanosecond.String(), conn.RemoteAddr(), conn.RemoteAddr().Network()), name: msg, channel: c }
+			go messageHandler(c, conn)
+			subscribe <- user
+			status = Joined
+		} else if status == Joined {
+			publish <- Event{userId: user.id, name: user.name, message: msg}
+
+		}
+		
+	}
+	
+	if status == Joined {
+		unsubscribe <- user
+	}
+
+}
+const (
+	UserListTemplate = "* This room contains %s"
+	UserEnterTemplate = "* %s has entered the room"
+	UserLeaveTemplate = "* %s has left the room"
+	NewMessageTemplate = "[%s] %s"
+)
+
+func messageHandler(c chan Event, conn net.Conn) {
+	for {
+		select {
+		case event := <- c:
+			var message string
+			if event.eventType == UserList {
+				message = fmt.Sprintf(UserListTemplate, event.message)
+
+			} else if event.eventType == NewUserBroadCast {
+				message = fmt.Sprintf(UserEnterTemplate, event.message)
+
+			} else if event.eventType == UserLeave {
+				message = fmt.Sprintf(UserLeaveTemplate, event.message)
+
+			} else if event.eventType == NewMessage {
+				message = fmt.Sprintf(NewMessageTemplate, event.name, event.message)
+
+			}
+
+			output := []byte(message)
+			output = append(output, '\n')
+			conn.Write(output)
+
+		}
+	}
 }
